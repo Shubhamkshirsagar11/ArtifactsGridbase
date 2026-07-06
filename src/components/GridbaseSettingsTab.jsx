f8acc756480d569a2bda74181968ff402ef726ae
Shubhamkshirsagar11 <shubham.kshirsagar@nirmitee.io>
2026-07-06 11:18:14 +0530

    feat(gridbase-settings-tab): add Gridbase Settings Tab component and update App navigation

---
+++ src/components/GridbaseSettingsTab.jsx
@@ -0,0 +1,14 @@
+export default function GridbaseSettingsTab() {
+  return (
+    <iframe
+      src="/gridbase-settings-tab/index.html"
+      title="Gridbase Settings Tab"
+      style={{
+        width: '100%',
+        height: 'calc(100vh - 44px)',
+        border: 'none',
+        display: 'block',
+      }}
+    />
+  )
+}
